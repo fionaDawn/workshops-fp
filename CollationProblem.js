70fa2670844075701d2262892a43cc6888e4ffb6
Fiona <15187771+fionaDawn@users.noreply.github.com>
2021-12-09 23:31:13 +0800

    Added problem and renamed arguments

--- CollationProblem.js
+++ CollationProblem.js
@@ -1,3 +1,41 @@
+// THE PROBLEM:
+// collate_records(keys,records): Re-organize an array of records by specified properties. Arguments:
+
+// keys: An array of property names by which to collate the result.
+// records: An array of string tables (associative arrays).
+
+// Returns a string table of tables (up to length(keys) deep) of all source records 
+// collated as per keys. If keys is ['a', 'b'], the results will satisfy result[rec['a']][rec['b']] = rec for all records in source.
+
+// Example in Ruby notation ([a,b] is an array; {k=>v} is a hash table):
+
+// keys = [ 'region', 'name' ]
+// records = [
+//   { 'id' => 1, 'name' => 'obj-1', 'region' => 'us', ... },
+//   { 'id' => 2, 'name' => 'obj-2', 'region' => 'us', ... },
+//   { 'id' => 3, 'name' => 'obj-3', 'region' => 'eu', ... },
+// ]
+
+// collate_records(keys, records)
+// returns
+
+// {
+//   'us' => {
+// 	'obj-1' => { 'id' => 1, 'name' => 'obj-1', 'region' => 'us', ... },
+// 	'obj-2' => { 'id' => 2, 'name' => 'obj-2', 'region' => 'us', ... },
+//    },
+//   'eu' => {
+// 	'obj-3' => { 'id' => 3, 'name' => 'obj-3', 'region' => 'eu', ... },
+//    },
+// }
+
+// ********************************************************************
+// ********************************************************************
+//                    SOLUTION STARTS HERE
+// ********************************************************************
+// ********************************************************************
+
+
 // needed to print nested maps
 const util = require('util')
 
@@ -15,28 +53,28 @@ const util = require('util')
 // this function simply groups a list by key
 // it checks if list is object or array since we want 
 // to return an object for single record, otherwise return array
-const group_by = (key, list) => {
+const group_by = (key, records) => {
     // we need to check if record is an array value
-    if (!list.length) {
-        return list[key] ? new Map([[list[key], list]]) : list;
+    if (!records.length) {
+        return records[key] ? new Map([[records[key], records]]) : records;
     }
-    return list.reduce((map, thisRecord, _i, remaining) => {
-        if (!thisRecord[key]) {
+    return records.reduce((map, currentRecord, _i, remaining) => {
+        if (!currentRecord[key]) {
             // if key doesn't exist retain a copy to return list
-            const retList = [...list]
+            const retList = [...records]
             // then break iteration do we don't go to the succeeding records of the list
             remaining.splice(0);
             return retList;
         }
 
-        const currRecord = map.get(thisRecord[key]);
-        if (currRecord) {
+        const thisRecord = map.get(currentRecord[key]);
+        if (thisRecord) {
             // if record is found, check if value is array
-            if (currRecord.length) currRecord.push(thisRecord)
-            else map.set(thisRecord[key], [currRecord, thisRecord])
+            if (thisRecord.length) thisRecord.push(currentRecord)
+            else map.set(currentRecord[key], [thisRecord, currentRecord])
         } else {
             // otherwise just set the record
-            map.set(thisRecord[key], thisRecord)
+            map.set(currentRecord[key], currentRecord)
         }
         return map;
         // used Map is default value since this is the HashTable equivalent of javascript
